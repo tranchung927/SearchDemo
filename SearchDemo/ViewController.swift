213b59e98af8729dc4584eab976204a10e60ecf1
ChungTran <chungtv@tadi.io>
2017-11-02 14:12:48 +0700

    Initial Commit

---
+++ SearchDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchDemo
+//
+//  Created by ChungTran on 11/2/17.
+//  Copyright © 2017 ChungTran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
